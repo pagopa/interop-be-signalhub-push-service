112b0645c8c836098b63f020717f6d9a24771785
DIR\luca.ciccarelli <luca.ciccarelli@accenture.com>
2023-09-13 17:19:49 +0200

    start project structure

---
+++ src/main/java/it/pagopa/interop/signalhub/push/service/SignalHubPushServiceApplication.java
@@ -0,0 +1,20 @@
+package it.pagopa.interop.signalhub.push.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+
+@Slf4j
+@SpringBootApplication
+@EnableR2dbcAuditing
+public class SignalHubPushServiceApplication {
+
+	public static void main(String[] args) {
+
+		log.error("AVAILABLE PROCESSOR: {}", Runtime.getRuntime().availableProcessors() );
+
+		SpringApplication.run(SignalHubPushServiceApplication.class, args);
+	}
+
+}
