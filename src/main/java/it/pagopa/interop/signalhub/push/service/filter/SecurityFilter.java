d25a1956ab1c93218cbc53053adb7640b0dc2519
DIR\luca.ciccarelli <luca.ciccarelli@accenture.com>
2023-09-14 12:47:22 +0200

    added structure filter for verify token parameters

---
+++ src/main/java/it/pagopa/interop/signalhub/push/service/filter/SecurityFilter.java
@@ -0,0 +1,36 @@
+package it.pagopa.interop.signalhub.push.service.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.AllArgsConstructor;
+import lombok.CustomLog;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+@CustomLog
+@Component
+@AllArgsConstructor
+public class SecurityFilter implements WebFilter {
+
+    private static final String TYPE = "at+jwt";
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        // Estrazione del token Bearer dall'header di autenticazione
+        String authorizationHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            // Rimuovere "Bearer " dal token
+            String token = authorizationHeader.substring(7);
+            if (token.isEmpty()){
+                // gestire eccezione
+            }
+            DecodedJWT decodedJWT = JWT.decode(token);
+            //String typ = decodedJWT.getClaim("typ").asString();
+            //controlli sui parametri del token.
+        }
+
+        // Continua con la catena dei filtri
+        return chain.filter(exchange);
+    }
+}
