7084331f43fbd0cbd04dba123fdd1280d0e00c46
cristopher.moccia <cristopher.moccia@accenture.com>
2023-09-15 09:24:02 +0200

    added test

---
+++ src/test/java/it/pagopa/interop/signalhub/push/service/SignalHubPushServiceTest.java
@@ -0,0 +1,15 @@
+package it.pagopa.interop.signalhub.push.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SignalHubPushServiceTest {
+
+
+    @Test
+    void testApp(){
+        assertTrue(true);
+    }
+
+}
