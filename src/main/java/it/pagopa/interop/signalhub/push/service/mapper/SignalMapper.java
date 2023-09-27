381abf51fec8cdfafe299a89708a27f5cad38a37
cristopher.moccia <cristopher.moccia@accenture.com>
2023-09-27 10:53:25 +0200

    minor changes

--- src/main/java/it/pagopa/interop/signalhub/push/service/mapper/SignalMapper.java
+++ src/main/java/it/pagopa/interop/signalhub/push/service/mapper/SignalMapper.java
@@ -2,11 +2,9 @@ package it.pagopa.interop.signalhub.push.service.mapper;
 
 import it.pagopa.interop.signalhub.push.service.dto.Signal;
 import it.pagopa.interop.signalhub.push.service.dto.SignalRequest;
-import it.pagopa.interop.signalhub.push.service.entities.EService;
 import it.pagopa.interop.signalhub.push.service.queue.model.SignalEvent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 public interface SignalMapper {
