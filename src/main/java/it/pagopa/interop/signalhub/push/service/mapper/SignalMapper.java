3615f7d756d2d00ce3d86dbc57f1054d2934a6c5
DIR\noemi.luongo <noemi.luongo@accenture.com>
2023-09-27 10:22:55 +0200

    minimal fix

--- src/main/java/it/pagopa/interop/signalhub/push/service/mapper/SignalMapper.java
+++ src/main/java/it/pagopa/interop/signalhub/push/service/mapper/SignalMapper.java
@@ -2,13 +2,18 @@ package it.pagopa.interop.signalhub.push.service.mapper;
 
 import it.pagopa.interop.signalhub.push.service.dto.Signal;
 import it.pagopa.interop.signalhub.push.service.dto.SignalRequest;
+import it.pagopa.interop.signalhub.push.service.entities.EService;
 import it.pagopa.interop.signalhub.push.service.queue.model.SignalEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 public interface SignalMapper {
 
     SignalEvent toEvent(SignalRequest signal);
+
+    @Mapping(target = "signalId", source= "signalRequest.indexSignal")
     Signal toSignal(SignalRequest signalRequest);
 
 }
