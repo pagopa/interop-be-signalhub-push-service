d68e5b6e7c39638f0a2dd275e2c5840a90356db9
DIR\noemi.luongo <noemi.luongo@accenture.com>
2023-09-29 16:26:59 +0200

    add junit

---
+++ src/test/java/it/pagopa/interop/signalhub/push/service/mapper/SignalMapperTest.java
@@ -0,0 +1,35 @@
+package it.pagopa.interop.signalhub.push.service.mapper;
+
+import it.pagopa.interop.signalhub.push.service.dto.Signal;
+import it.pagopa.interop.signalhub.push.service.dto.SignalRequest;
+import it.pagopa.interop.signalhub.push.service.queue.model.SignalEvent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mapstruct.factory.Mappers;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+class SignalMapperTest {
+
+
+    private SignalMapper service = Mappers.getMapper(SignalMapper.class);
+
+    @Test
+    void toEvent() {
+        SignalRequest signalRequest= new SignalRequest();
+        signalRequest.setIndexSignal(1L);
+        SignalEvent signal= service.toEvent(signalRequest);
+        assertEquals(signalRequest.getIndexSignal(), signal.getIndexSignal() );
+    }
+
+    @Test
+    void toSignal() {
+        SignalRequest signalRequest= new SignalRequest();
+        signalRequest.setIndexSignal(1L);
+        Signal signal= service.toSignal(signalRequest);
+        assertEquals(signal.getSignalId(), signalRequest.getIndexSignal());
+    }
+}
