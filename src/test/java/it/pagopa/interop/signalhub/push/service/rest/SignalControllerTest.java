88cfc7a1e22af3fd584534b27e68afa78f33b0af
DIR\noemi.luongo <noemi.luongo@accenture.com>
2023-09-29 16:06:24 +0200

    minimal fix

--- src/test/java/it/pagopa/interop/signalhub/push/service/rest/SignalControllerTest.java
+++ src/test/java/it/pagopa/interop/signalhub/push/service/rest/SignalControllerTest.java
@@ -48,7 +48,7 @@ class SignalControllerTest extends BaseTest.WithWebEnvironment {
         SignalRequest request = new SignalRequest();
         request.setSignalType(SignalType.CREATE);
         request.setEserviceId("123");
-        request.setIndexSignal(Long.valueOf(1));
+        request.setIndexSignal(Long.valueOf(2));
         request.setObjectId("test");
         request.setObjectType("test");
         return request;
--- src/test/resources/data-h2.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO organization_eservice (eservice_id, organization_id, state, tmst_insert, tmst_last_edit)
-VALUES ('ABC-123', '84871fd4-2fd7-46ab-9d22-f6b452f4b3c5', 'OK', now(), now());
