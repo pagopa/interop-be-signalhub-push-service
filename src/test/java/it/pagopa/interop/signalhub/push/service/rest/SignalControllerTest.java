3b910d637e78a745062cfcd8c7639aa030f64303
cristopher.moccia <cristopher.moccia@accenture.com>
2023-10-16 16:53:38 +0200

    fixes properties

--- src/test/java/it/pagopa/interop/signalhub/push/service/rest/SignalControllerTest.java
+++ src/test/java/it/pagopa/interop/signalhub/push/service/rest/SignalControllerTest.java
@@ -11,8 +11,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithSecurityContext;
 import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
