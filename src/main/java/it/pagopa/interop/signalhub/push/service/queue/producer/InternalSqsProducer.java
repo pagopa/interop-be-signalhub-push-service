45fa68c3224f16761c073fdcec4667caae788043
cristopher.moccia <cristopher.moccia@accenture.com>
2023-11-07 09:56:58 +0100

    Revert "fixed type of correlationId"
    This reverts commit 328277c971585dcc930b20c37e73975fab15c82d.

--- src/main/java/it/pagopa/interop/signalhub/push/service/queue/producer/InternalSqsProducer.java
+++ src/main/java/it/pagopa/interop/signalhub/push/service/queue/producer/InternalSqsProducer.java
@@ -1,10 +1,12 @@
 package it.pagopa.interop.signalhub.push.service.queue.producer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.awspring.cloud.sqs.operations.SendResult;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import it.pagopa.interop.signalhub.push.service.queue.model.SignalEvent;
 import it.pagopa.interop.signalhub.push.service.utils.Utility;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,9 @@ import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @AllArgsConstructor
@@ -23,7 +28,7 @@ public class InternalSqsProducer implements SqsProducer<SignalEvent> {
     @Override
     public Mono<SignalEvent> push(SignalEvent event) {
         Message<String> message = MessageBuilder.withPayload(convertToJson(event))
-                .setHeader("correlationId", UUID.randomUUID().toString()).build();
+                .setHeader("correlationId", Math.random()).build();
         return Mono.fromFuture(this.sqsTemplate.sendAsync(message))
                 .thenReturn(event);
     }
