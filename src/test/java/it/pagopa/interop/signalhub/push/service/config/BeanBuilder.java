ec9a3f3a561e405fe15131e295f32e1892b67122
cristopher.moccia <cristopher.moccia@accenture.com>
2023-11-09 21:18:58 +0100

    fixed some test

--- src/test/java/it/pagopa/interop/signalhub/push/service/config/BeanBuilder.java
+++ src/test/java/it/pagopa/interop/signalhub/push/service/config/BeanBuilder.java
@@ -1,8 +1,21 @@
 package it.pagopa.interop.signalhub.push.service.config;
 
 import it.pagopa.interop.signalhub.push.service.auth.PrincipalAgreement;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Slf4j
+@Configuration
 public class BeanBuilder {
+
+
+    @Bean
+    public MockServer getMockServer(@Value("${mockserver.bean.port}") int port){
+        log.info("Port :  {}", port);
+        return new MockServer(port);
+    }
 
     public static PrincipalAgreement getPrincipal(){
         PrincipalAgreement p = new PrincipalAgreement();
