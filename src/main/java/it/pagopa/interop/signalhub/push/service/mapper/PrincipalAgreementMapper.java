c9f07f3b60d23fd9a59eee2675e2a89c29be0eb7
cristopher.moccia <cristopher.moccia@accenture.com>
2023-11-09 09:40:00 +0100

    added mapping properties

--- src/main/java/it/pagopa/interop/signalhub/push/service/mapper/PrincipalAgreementMapper.java
+++ src/main/java/it/pagopa/interop/signalhub/push/service/mapper/PrincipalAgreementMapper.java
@@ -4,10 +4,14 @@ package it.pagopa.interop.signalhub.push.service.mapper;
 import it.pagopa.interop.signalhub.push.service.auth.PrincipalAgreement;
 import it.pagopa.interop.signalhub.push.service.generated.openapi.client.interop.model.v1.Agreement;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PrincipalAgreementMapper {
 
+    @Mapping(target = "purposeId", source= "agreement.id")
+    @Mapping(target = "principalId", source= "agreement.consumerId")
+    @Mapping(target = "EServiceId", source= "agreement.eserviceId")
     PrincipalAgreement toPrincipal(Agreement agreement);
 
 
