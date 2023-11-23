250fc76c28ef938bd1150eb7db3d256146d8a14b
DIR\noemi.luongo <noemi.luongo@accenture.com>
2023-11-23 12:31:39 +0100

    fix JsonException

--- src/main/java/it/pagopa/interop/signalhub/push/service/mapper/ProblemErrorMapper.java
+++ src/main/java/it/pagopa/interop/signalhub/push/service/mapper/ProblemErrorMapper.java
@@ -1,9 +1,6 @@
 package it.pagopa.interop.signalhub.push.service.mapper;
 
 import it.pagopa.interop.signalhub.push.service.dto.ProblemError;
-import it.pagopa.interop.signalhub.push.service.dto.Signal;
-import it.pagopa.interop.signalhub.push.service.dto.SignalRequest;
-import it.pagopa.interop.signalhub.push.service.queue.model.SignalEvent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.validation.FieldError;
@@ -13,7 +10,7 @@ public interface ProblemErrorMapper {
 
     @Mapping(target = "code", source= "signal.field")
     @Mapping(target = "detail", source= "signal.defaultMessage")
-    ProblemError toEvent(FieldError signal);
+    ProblemError toProblemError(FieldError signal);
 
 
 
